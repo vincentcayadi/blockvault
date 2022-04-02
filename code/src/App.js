95d111be482d73d21fadfbb19bf096e16b18dcfe
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-04-02 22:42:01 +0800

    Added Asher's changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,8 @@
-import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 
 export default function App() {
   return (
     <>
-      <Navbar />
       <Hero/>
     </>
   )
--- code/src/components/Hero.js
+++ code/src/components/Hero.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import creditCard from './creditcard.svg'
+import React from "react";
+import creditCard from "./creditcard.svg";
 
 export const Hero = () => {
     return (
-        <>
-            <section class="bg-nord6 select-none font-mono subpixel-antialiased">
-                <div class="containter flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
-                    <div class='flex flex-1 flex-col items-center ig:items-start'>
-                        <h2 class='text-nord1 text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6'>
-                            Keep your data secure
-                        </h2>
-                        <p class="text-nord2 text-lg text-center lg:text-left mb-6">
-                            Store your data securely on the decentralised Blockchain network using Blockvault.
-                        </p>
-                        <button class="subpixel-antialiased px-4 py-2 font-semibold text-xl bg-blue-500 text-white rounded-md shadow-sm hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ease-in-out delay-150 duration-300">
-                            Enter App
-                        </button>
-                    </div>
-                    <div class="flex flex-2 flex-col items-center ig:items-start">
-                        <img src={creditCard} alt="credit card" class="w-full h-auto mb-6" />
-                    </div>
+    <>
+        <section class="bg-nord6 select-none font-mono subpixel-antialiased">
+            <div class="containter flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
+                <div class="flex flex-1 flex-col items-center ig:items-start p-10 justify-start">
+                <h2 class="text-nord1 text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6 w-full">
+                    Keep your data secure
+                </h2>
+                <p class="text-nord2 text-lg text-center lg:text-left mb-6">
+                    Store your data securely on the decentralised Blockchain network
+                    using Blockvault.
+                </p>
+                <button class="subpixel-antialiased px-4 py-2 font-semibold text-xl bg-blue-500 text-white rounded-md shadow-sm hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 ease-in-out delay-150 duration-300">
+                    Enter App
+                </button>
                 </div>
-            </section>
-        </>
-            
+                <div class="flex flex-2 flex-col items-center ig:items-start w-1/2">
+                <img
+                    src={creditCard}
+                    alt="credit card"
+                    class="w-full h-auto mb-6"
+                />
+                </div>
+            </div>
+        </section>
+    </>
+    );
+};
 
-    )
-}
-
-export default Hero
+export default Hero;
