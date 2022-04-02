28ca5e1c0337c45574e4083cba673338c8bf4926
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-04-02 11:18:43 +0800

    Navbar

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Navbar from '../src/components/Navbar.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>yo</h1>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+      <Routes>
+      <Route path='/' exact/>
+      </Routes>
+    </Router>
+);
 }
 
 export default App;
