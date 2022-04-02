a43a6eeb5292381f7208b127f66e65e9552595b1
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-04-02 16:57:38 +0800

    changed to createRoot

--- code/src/index.js
+++ code/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
+
