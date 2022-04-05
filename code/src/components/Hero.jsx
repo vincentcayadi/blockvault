510ae4bb0ac7a90662651c992e7fbe030b5d919c
ashergwee <ashergwee@gmail.com>
2022-04-05 19:58:46 +0800

    minor changes to the css of landing

--- code/src/components/Hero.jsx
+++ code/src/components/Hero.jsx
@@ -37,7 +37,7 @@ export const Hero = () => {
           <h1 class="text-3xl md:text-4 lg:text-4xl text-center mb-6 font-bold">Say hello to decentralized storage</h1>
           <p class=" lg:text-xl text-center"> Storing data on a centralized server is outdated</p>
           <p class=" lg:text-xl text-center mb-6"> It is time to store your data on the Blockchain</p>
-          <button class="bg-nord3 rounded-xl text-nord6 p-4 text-sm w-1/4 m-auto mb-12"> {/* Style this better pls*/}
+          <button class="bg-nord3 rounded-xl text-nord6 p-4 text-sm w-1/4 m-auto mb-12 hover:bg-transparent hover:text-nord3 hover:border-2 hover:border-nord3"> {/* Style this better pls*/}
             Learn more
           </button>
         </div>
@@ -46,7 +46,7 @@ export const Hero = () => {
       <p class="lg:text-xl text-center mb-6">BlockVault provides support for some commonly use crypto wallets for authentication and login</p>      
       <section class="relative">
         <div class="grid grid-cols-2 gap-14">
-          <div class="border-2 rounded-xl p-12 h-96 text-left relative hover:-translate-y-2 duration-300 hover:shadow">
+          <div class="border-2 rounded-xl p-12 h-96 text-left relative hover:-translate-y-2 duration-300 hover:shadow bg-nord5">
             <h1 class="font-medium text-2xl mb-6">
               Metamask (Etherium)
             </h1>
@@ -59,7 +59,7 @@ export const Hero = () => {
               </button>
             </div>
           </div>
-          <div class="border-2 rounded-xl p-12 h-96 text-left relative hover:-translate-y-1 duration-300 hover:shadow">
+          <div class="border-2 rounded-xl p-12 h-96 text-left relative hover:-translate-y-1 duration-300 hover:shadow bg-nord5">
             <h1 class="font-medium text-2xl mb-6">
               Phantom (Solana)
             </h1>
