fe077d300d5de2950255c4a5b344ad0684274dc4
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-07-04 14:07:15 +0800

    redid button structure

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,18 +10,18 @@ const NotFoundPage = () => {
             <h1 className="font-extrabold text-8xl">404</h1>
             <h2>Page Not Found</h2>
           </div>
-          <div className="px-2 mt-12">
+          <div className="grid grid-cols-2 gap-4 mt-2">
             <Link
               exact
               to="/"
-              className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
+              className="p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
             >
               Homepage
             </Link>
             <Link
               exact
               to="/dashboard"
-              className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
+              className="p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
             >
               Dashboard
             </Link>
