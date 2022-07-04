3a5aea96098bcee48fada808dfac3b3abcdf870e
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-07-04 13:42:56 +0800

    formatted 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 const NotFoundPage = () => {
   return (
     <>
-      <Navbar />
-      <body className="container">
-        <h1>NOT FOUND</h1>
-      </body>
+      <div>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Sorry, the content you were looking for doesnt exist</p>
+        <p>It was either removed, or there is a typo in the link</p>
+        <Link to="/">Homepage</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </div>
       <Footer />
     </>
   );
