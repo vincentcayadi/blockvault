d602b8245c539fb997de8736159dc763942e1c5f
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-07-04 14:05:31 +0800

    done with buttons

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
 
 const NotFoundPage = () => {
   return (
     <>
-      <div>
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
-        <p>Sorry, the content you were looking for doesnt exist</p>
-        <p>It was either removed, or there is a typo in the link</p>
-        <Link to="/">Homepage</Link>
-        <Link to="/dashboard">Dashboard</Link>
+      <div className="grid w-full h-screen text-center place-items-center">
+        <div>
+          <div>
+            <h1 className="font-extrabold text-8xl">404</h1>
+            <h2>Page Not Found</h2>
+          </div>
+          <div className="px-2 mt-12">
+            <Link
+              exact
+              to="/"
+              className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
+            >
+              Homepage
+            </Link>
+            <Link
+              exact
+              to="/dashboard"
+              className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 border-2 bg-nord2 rounded-xl hover:scale-110"
+            >
+              Dashboard
+            </Link>
+          </div>
+        </div>
       </div>
-      <Footer />
     </>
   );
 };
--- src/sections/LM.jsx
+++ src/sections/LM.jsx
@@ -5,17 +5,17 @@ export default function LearnMore() {
     <>
       <section className="relative mb-60 lg:mb-80">
         <div className="flex flex-col">
-          <h1 className="mb-6 text-3xl font-bold text-center md:text-4 lg:text-4xl text-nord0 dark:text-nord6">
+          <h1 className="mb-6 text-3xl font-bold text-center md:text-4 lg:text-4xl text-nord0 ">
             Say hello to decentralized storage
           </h1>
-          <p className="text-center lg:text-xl text-nord3 dark:text-nord5">
+          <p className="text-center lg:text-xl text-nord3">
             Storing data on a centralized server is outdated
           </p>
-          <p className="mb-6 text-center lg:text-xl text-nord3 dark:text-nord5">
+          <p className="mb-6 text-center lg:text-xl text-nord3">
             It is time to store your data on the Blockchain
           </p>
           <a
-            className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 bg-nord2 rounded-xl hover:bg-transparent hover:text-nord2 hover:border-2 hover:border-nord2 hover:scale-110 dark:hover:bg-nord6 dark:border-0"
+            className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 bg-nord2 rounded-xl hover:bg-transparent hover:text-nord2 hover:border-2 hover:border-nord2 hover:scale-110"
             href="https://vincent-cayadi.github.io/blockvault/"
           >
             Learn more
