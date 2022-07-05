cd87506a6419ac70c99b83aaee1a2aed65de5c67
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-07-05 08:20:59 +0800

    fixed margin issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <div className="container px-12 py-4 mx-auto my-12 border-b-2 select-none bg-nord6 max-w-s font-mr border-nord5 dark:bg-nord0">
+      <div className="container px-12 py-4 mx-auto mt-12 border-b-2 select-none bg-nord6 max-w-s font-mr border-nord5 dark:bg-nord0">
         <p className="inline-block text-sm font-bold dark:text-nord6">
           BlockVault
         </p>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,13 @@ import Hero from '../sections/Hero';
 import Footer from '../components/Footer';
 import NavBar from '../components/Navbar-landing';
 import landingLight from '../images/landingDesign.svg';
-import landingDark from '../images/landingDesignDark.svg';
 import hexagonOneLight from '../images/landinghexagon1Light.svg';
 import hexagonTwoLight from '../images/landinghexagon2Light.svg';
-import hexagonOneDark from '../images/landingHexagon1Dark.svg';
-import hexagonTwoDark from '../images/landingHexagon2Dark.svg';
 
 export default function Index() {
   return (
     <>
-      <div className="absolute top-0 right-0 bg-nord6 sm:hidden">
+      <div className="absolute top-0 right-0 bg-nord6 sm:hidden ">
         <img src={landingLight} alt="landingDesign" />
       </div>
 
