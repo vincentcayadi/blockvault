7e32643e6be525721d39561dc934dbe393441f7b
ashergwee <ashergwee@gmail.com>
2022-07-06 18:42:34 +0800

    more changes to the logo in 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import landingLight from '../images/landingDesign.svg';
 import hexagonOneLight from '../images/landinghexagon1Light.svg';
 import logo from '../graphics/icon/black-icon.png';
+import logotext from '../graphics/icontext/black-icontext.png'
 // import hexagonTwoLight from '../images/landinghexagon2Light.svg';
 
 const NotFoundPage = () => {
@@ -20,18 +21,17 @@ const NotFoundPage = () => {
         {/* <div className="absolute bottom-0 bg-nord6 sm:hidden z-0 left-0">
           <img src={hexagonTwoLight} alt="hexagon2" />
         </div> */}
+  
+        <img src={logotext} alt="logo" className="w-1/6 sm:hidden z-10 absolute left-16 top-16" />
         
         <div className="z-10">
-          <div className="flex w-full justify-center">
-            <img src={logo} alt="logo" className="w-1/12 self-center" />
-          </div>
           <h1 className="font-extrabold text-9xl my-3">404</h1>
           <h2 className="font-semibold text-5xl my-3">Page Not Found</h2>
           <p className="text-xl mt-6 font-light">Sorry, the content you are looking for does not exist.</p>
           <p className="text-xl font-light">It was either removed, or there was a typo in the link.</p>
 
           <div className="flex justify-center">
-            <div className="flex w-1/2 justify-between content-center my-6">
+            <div className="flex w-full justify-between content-center my-6">
               <Link 
                 exact 
                 to="/" 
