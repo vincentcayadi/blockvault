7fee60de39e161f94c0e32233bcf149d0ef4f6ed
ashergwee <ashergwee@gmail.com>
2022-07-06 18:23:02 +0800

    added logo to 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import landingLight from '../images/landingDesign.svg';
 import hexagonOneLight from '../images/landinghexagon1Light.svg';
+import logo from '../graphics/icon/black-icon.png';
 // import hexagonTwoLight from '../images/landinghexagon2Light.svg';
 
 const NotFoundPage = () => {
   return (
     <>     
-      <div className="grid w-full h-screen text-center place-items-center z-10 bg-nord6 font-manrope">
+      <div className="grid w-full h-screen text-center place-items-center z-10 bg-nord6 font-manrope">    
         <div className="absolute top-0 right-0 bg-nord6 sm:hidden z-0">
           <img src={landingLight} alt="landingDesign" />
         </div>
@@ -21,28 +22,33 @@ const NotFoundPage = () => {
         </div> */}
         
         <div className="z-10">
-
+          <div className="flex w-full justify-center">
+            <img src={logo} alt="logo" className="w-1/12 self-center" />
+          </div>
           <h1 className="font-extrabold text-9xl my-3">404</h1>
           <h2 className="font-semibold text-5xl my-3">Page Not Found</h2>
           <p className="text-xl mt-6 font-light">Sorry, the content you are looking for does not exist.</p>
           <p className="text-xl font-light">It was either removed, or there was a typo in the link.</p>
 
-          <div className="flex w-full justify-between content-center my-6">
-            <Link 
-              exact 
-              to="/" 
-              className="w-1/3 p-4 text-center bg-nord3 rounded-xl text-nord6 mx-auto"
-            >
-              Homepage
-            </Link>
-            <Link 
-              exact 
-              to="/dashboard" 
-              className="w-1/3 p-4 text-center bg-nord3 rounded-xl text-nord6 mx-auto"
-            >
-              Dashboard
-            </Link>
+          <div className="flex justify-center">
+            <div className="flex w-1/2 justify-between content-center my-6">
+              <Link 
+                exact 
+                to="/" 
+                className="w-1/3 p-4 text-center bg-nord3 rounded-xl text-nord6 mx-auto"
+              >
+                Homepage
+              </Link>
+              <Link 
+                exact 
+                to="/dashboard" 
+                className="w-1/3 p-4 text-center bg-nord3 rounded-xl text-nord6 mx-auto"
+              >
+                Dashboard
+              </Link>
+            </div>
           </div>
+          
         </div>
       </div>
     </>
