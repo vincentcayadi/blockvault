b55627edc44a1310739c1b5f2b15a7d6abc06250
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-11 16:10:51 +0800

    Edited Learn More

--- src/sections/LM.jsx
+++ src/sections/LM.jsx
@@ -14,12 +14,44 @@ export default function LearnMore() {
           <p className="mb-6 text-center lg:text-xl text-nord3">
             It is time to store your data on the Blockchain
           </p>
-          <a
-            className="w-1/4 p-4 mx-auto text-sm text-center text-white duration-300 bg-nord2 rounded-xl hover:bg-transparent hover:text-nord2 hover:border-2 hover:border-nord2 hover:scale-110"
-            href="https://vincent-cayadi.github.io/blockvault/"
-          >
-            Learn more
-          </a>
+          <div className="grid h-full grid-cols-3 gap-4">
+            <div className="p-4 rounded-md shadow-md bg-nord5">
+              <div className="grid w-16 h-16 p-4 text-center rounded-full shadow-md bg-nord4 place-items-center">
+                <p className="text-2xl font-semibold">1</p>
+              </div>
+              <p className="mt-4 mb-2 text-xl font-bold">Authenticate</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="p-4 rounded-md shadow-md bg-nord5">
+              <div className="grid w-16 h-16 p-4 text-center rounded-full shadow-md bg-nord4 place-items-center">
+                <p className="text-2xl font-semibold">2</p>
+              </div>
+              <p className="mt-4 mb-2 text-xl font-bold">Encrypt</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+            <div className="p-4 rounded-md shadow-md bg-nord5">
+              <div className="grid w-16 h-16 p-4 text-center rounded-full shadow-md bg-nord4 place-items-center">
+                <p className="text-2xl font-semibold">3</p>
+              </div>
+              <p className="mt-4 mb-2 text-xl font-bold">Upload</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
