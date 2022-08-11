01e9d1c6318cda9db957fb1f88e42dc70d9c2dbb
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-11 18:25:57 +0800

    Added blur to upload

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -14,10 +14,7 @@ export const dete = [];
 
 const fileTypes = ['JPEG', 'PNG', 'GIF'];
 
-
 var CryptoJS = require('crypto-js');
-
-
 
 const Upload = (props) => {
   var fileInput, hashFileInput;
@@ -30,14 +27,30 @@ const Upload = (props) => {
 
   const [file] = useState(null);
 
-  
-  function setCookie(cname, cvalue, cformat, csize, exdays, imageKeyHash, fileName) {
-    console.log("-----------------setCookie-----------------");
+  function setCookie(
+    cname,
+    cvalue,
+    cformat,
+    csize,
+    exdays,
+    imageKeyHash,
+    fileName
+  ) {
+    console.log('-----------------setCookie-----------------');
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     let expires = 'expires=' + d.toUTCString();
 
-    cvalue = cvalue + ',' + fileName + ',' + cformat + ',' + csize + ',' + 'NotUpdated';
+    cvalue =
+      cvalue +
+      ',' +
+      fileName +
+      ',' +
+      cformat +
+      ',' +
+      csize +
+      ',' +
+      'NotUpdated';
 
     var cookieKey = CryptoJS.RIPEMD160(imageKeyHash).toString();
 
@@ -57,7 +70,7 @@ const Upload = (props) => {
 
   async function upload() {
     updateData(imageKeyHash);
-    console.log("-----------------upload-----------------");
+    console.log('-----------------upload-----------------');
     console.log(fileInput);
     const data = fileInput;
     const file = new Moralis.File(data.name, data);
@@ -86,15 +99,21 @@ const Upload = (props) => {
       csize = data.size + ' B';
     }
 
-    console.log("Cookie Name: " + cookieName);
-    console.log("Cookie Link: " + file.ipfs());
+    console.log('Cookie Name: ' + cookieName);
+    console.log('Cookie Link: ' + file.ipfs());
     // console.log("Cookie type: " + data.type);``
-    console.log("Cookie Size: " + csize);
-    console.log("Cookie Hash: " + imageKeyHash);
+    console.log('Cookie Size: ' + csize);
+    console.log('Cookie Hash: ' + imageKeyHash);
 
-    var cookieKey = setCookie(cookieName, file.ipfs(), data.type, csize, 1, imageKeyHash, data.name);
-
-    
+    var cookieKey = setCookie(
+      cookieName,
+      file.ipfs(),
+      data.type,
+      csize,
+      1,
+      imageKeyHash,
+      data.name
+    );
 
     getData(cookieName, cookieKey);
   }
@@ -106,9 +125,7 @@ const Upload = (props) => {
   // }
 
   function getData(cname, cookieKey) {
-    
-
-    console.log("-----------------getData-----------------");
+    console.log('-----------------getData-----------------');
 
     let x = document.cookie;
     let ca = x.split(';');
@@ -123,33 +140,32 @@ const Upload = (props) => {
       // }
       // c = c.substring(1);
 
-
       let c = ca[i];
       let e = 0;
       while (c.charAt(e) !== '=') {
         e++;
       }
       console.log(c.substring(1, e));
-      
-      if (c.substring(1, e) === cname ) {
+
+      if (c.substring(1, e) === cname) {
         console.log('cookie found');
         console.log(c);
         console.log(cookieKey);
-        
+
         let cD = c.substring(e + 1);
         console.log(cD);
-        var decryptedCookie = CryptoJS.AES.decrypt(cD, cookieKey).toString(CryptoJS.enc.Utf8);
-        
+        var decryptedCookie = CryptoJS.AES.decrypt(cD, cookieKey).toString(
+          CryptoJS.enc.Utf8
+        );
+
         console.log(decryptedCookie);
-        
-        
+
         // let n = 0;
         // while (decryptedCookie.charAt(n) !== ',') {                   // get file link
         //   n = n + 1;
         // }
         // let cfileLink = decryptedCookie.substring(0, n);
 
-        
         // let m = n + 1;
         // while (decryptedCookie.charAt(m) !== ',') {                   // get file name
         //   m = m + 1;
@@ -162,23 +178,30 @@ const Upload = (props) => {
         // }
         // let cformat = decryptedCookie.substring(m + 1, o);
 
-
         let dCD = decryptedCookie.split(',');
-        
+
         let cfileLink = dCD[0];
         let fileName = dCD[1];
         let cformat = dCD[2];
         let csize = dCD[3];
         let cstatus = dCD[4];
-        
-        
-        
-        console.log(cname + ':' + cfileLink + ',' + fileName + ',' + cformat + ',' + csize + ',' + cstatus);
+
+        console.log(
+          cname +
+            ':' +
+            cfileLink +
+            ',' +
+            fileName +
+            ',' +
+            cformat +
+            ',' +
+            csize +
+            ',' +
+            cstatus
+        );
         console.log(dete);
         window.open(cfileLink, '_blank');
-      }
-      
-      else {
+      } else {
         console.log('cookie not found');
       }
     }
@@ -214,35 +237,32 @@ const Upload = (props) => {
   //   };
 
   // }
-  
+
   function updateData(imageKeyHash) {
-    console.log("-----------------updateData-----------------");
+    console.log('-----------------updateData-----------------');
     if (fileHashed) {
-      
       let x = document.cookie;
-      
-      let xa = x.split(";");
+
+      let xa = x.split(';');
       console.log(xa.length);
-      
+
       for (let i = 0; i < xa.length; i++) {
-        
         let c = xa[i];
         let e = 0;
         while (c.charAt(e) !== '=') {
           e++;
         }
         console.log(c.substring(0, e));
-        
+
         if (c.substring(e + 1) !== imageKeyHash) {
-          console.log("Not ImageKeyHash");
+          console.log('Not ImageKeyHash');
           let cookie = {
-            image : '',
-            name : '',
-            format : '',
-            size : '',
+            image: '',
+            name: '',
+            format: '',
+            size: '',
           };
-        
-          
+
           let y = xa[i];
           console.log(y);
           while (y.charAt(0) !== '=') {
@@ -253,45 +273,50 @@ const Upload = (props) => {
           console.log(y);
           console.log(cookieKey);
           y = CryptoJS.AES.decrypt(y, cookieKey).toString(CryptoJS.enc.Utf8);
-        
-          let ya = y.split(",");
-        
+
+          let ya = y.split(',');
+
           console.log(ya);
-          
-          if (ya[4] === "NotUpdated") {
+
+          if (ya[4] === 'NotUpdated') {
             cookie.image = ya[0];
             cookie.name = ya[1];
             cookie.format = ya[2];
             cookie.size = ya[3];
-            
+
             console.log(cookie);
             dete.push(cookie);
-            
-            let updatedCookieData = ya[0] + ',' + ya[1] + ',' + ya[2] + ',' + ya[3] + ',' + 'Updated';
-            updatedCookieData = CryptoJS.AES.encrypt(updatedCookieData, cookieKey).toString();
+
+            let updatedCookieData =
+              ya[0] + ',' + ya[1] + ',' + ya[2] + ',' + ya[3] + ',' + 'Updated';
+            updatedCookieData = CryptoJS.AES.encrypt(
+              updatedCookieData,
+              cookieKey
+            ).toString();
             console.log(updatedCookieData);
             const d = new Date();
             d.setTime(d.getTime() + 1 * 24 * 60 * 60 * 1000);
             let expires = 'expires=' + d.toUTCString();
-            document.cookie = c.substring(0, e) + '=' + updatedCookieData + ";" + expires + ';path=/';
-        }
-      else {
-        console.log("ImageKeyHash");
+            document.cookie =
+              c.substring(0, e) +
+              '=' +
+              updatedCookieData +
+              ';' +
+              expires +
+              ';path=/';
+          } else {
+            console.log('ImageKeyHash');
+          }
         }
       }
-  
+    } else {
+      console.log('No file has been uploaded');
     }
   }
-  else {
-      console.log("No file has been uploaded");
-    }
-    
-  }
-
 
   return props.trigger ? (
-    <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-screen bg-black-rgba">
-      <div className="relative w-full max-w-2xl rounded-md bg-nord6">
+    <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-screen backdrop-blur-sm bg-nord4/50">
+      <div className="relative w-full max-w-2xl rounded-md bg-nord6 drop-shadow-lg">
         <AiFillCloseCircle
           className="absolute top-0 right-0 w-6 h-6 m-4 cursor-pointer text-nord1"
           onClick={() => props.setTrigger(false)}
@@ -312,9 +337,11 @@ const Upload = (props) => {
           </div> */}
 
           <section className="flex flex-col items-center p-5 mx-8 mb-8 border-2 border-dashed rounded-lg border-nord3 text-nord1 bg-nord5">
-            <div {...getRootProps({className:"dropzone"})}>
+            <div {...getRootProps({ className: 'dropzone' })}>
               <input {...getInputProps()} />
-              <p className="p-20">Drag and drop some files here, or click to select files</p>
+              <p className="p-20">
+                Drag and drop some files here, or click to select files
+              </p>
             </div>
           </section>
 
