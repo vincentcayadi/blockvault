3bfee4a2fabd198db57751ff00a6cf3869475f5b
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-08-13 15:45:26 +0800

    changed font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FiGithub } from 'react-icons/fi';
 const Footer = () => {
   return (
     <>
-      <div className="container px-12 py-4 mx-auto mt-12 border-b-2 select-none bg-nord6 max-w-s font-mr border-nord5">
+      <div className="px-12 py-4 mx-auto mt-12 border-b-2 select-none font-manrope bg-nord6 max-w-s font-mr border-nord5">
         <p className="inline-block text-sm font-bold ">BlockVault © 2022</p>
         <p className="inline-block px-4 text-xs text-nord2/25 ">
           Keep Your Data Secure
